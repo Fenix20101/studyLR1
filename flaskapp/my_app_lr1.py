78e8a08e95e7e9620ff1755ee7e1f697be1ba74c
Fenix20101 <Fenix20101@mail.ru>
2024-08-13 16:09:03 +0300

    Update my_app_lr1.py

--- flaskapp/my_app_lr1.py
+++ flaskapp/my_app_lr1.py
@@ -13,9 +13,8 @@ from flask import render_template
 def data_to():
  #создаем переменные с данными для передачи в шаблон
  some_pars = {'user':'Ivan','color':'red'}
-75
+
  some_str = 'Hello my dear friends!'
  some_value = 10
  #передаем данные в шаблон и вызываем его
- return render_template('simple.html',some_str = some_str,
- some_value = some_value,some_pars=some_pars) 
+ return render_template('simple.html',some_str = some_str, some_value = some_value,some_pars=some_pars) 
